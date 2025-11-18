4e7c8417e04bfebb4a21634713594e697dd5174b
Adrià Gomis <gomisroca@gmail.com>
2025-11-18 14:46:04 +0100

    Remove dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { ThemeProvider } from "next-themes";
-import NavPanel from "./ui/nav-panel";
 
 export const metadata: Metadata = {
   title: "SOKUU",
@@ -15,12 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="bg-lime-400 dark:bg-amber-400">
-        <ThemeProvider attribute="class">
-          <NavPanel />
-          {children}
-        </ThemeProvider>
-      </body>
+      <body className="bg-lime-400">{children}</body>
     </html>
   );
 }
