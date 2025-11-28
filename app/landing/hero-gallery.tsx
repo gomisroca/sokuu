7046de3ad852129ebd794824ac51432ea1891fef
Adrià Gomis <gomisroca@gmail.com>
2025-11-28 14:02:14 +0100

    simplify hero section out

--- app/landing/hero-gallery.tsx
+++ app/landing/hero-gallery.tsx
@@ -1,21 +1,23 @@
 "use client";
 
 import { useGSAP, gsap } from "@/gsap/setup";
+import dynamic from "next/dynamic";
 import Image from "next/image";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+const GallerySection1 = dynamic(() => import("./section-1"), {
+  ssr: false,
+});
 
 export default function HeroGallery() {
   const galleryRef = useRef<HTMLDivElement>(null);
+  const [showGallerySection1, setShowGallerySection1] = useState(false);
 
   useGSAP(() => {
     const sections = gsap.utils.toArray<HTMLElement>("#gallery-section");
 
-    const fadeInOffset = 150; // vh per section for fade-in
-    const fadeOutOffset = 250; // vh per section for fade-out
-    const fadeDistance = 200; // how far each fade happens
-    const fadeOutDelay = 1000; // extra scroll before fade-out starts
+    const scrollInOffset = 150;
+    const scrollDistance = 200;
 
-    // Fade-in sections (staggered)
     sections.forEach((section, index) => {
       gsap.fromTo(
         section,
@@ -26,35 +28,15 @@ export default function HeroGallery() {
           ease: "power2.out",
           scrollTrigger: {
             trigger: "#gallery-trigger",
-            start: `top+=${index * fadeInOffset}vh 85%`,
-            end: `+=${fadeDistance}vh`,
+            start: `top+=${index * scrollInOffset}vh 85%`,
+            end: `+=${scrollDistance}vh`,
             scrub: true,
+            onEnter:
+              index === 0 ? () => setShowGallerySection1(true) : undefined,
           },
         }
       );
     });
-
-    // Fade-out sections (reverse order, gradual)
-    sections
-      .slice()
-      .reverse()
-      .forEach((section, index) => {
-        gsap.to(section, {
-          autoAlpha: 0,
-          y: "100vh",
-          ease: "power2.in",
-          scrollTrigger: {
-            trigger: "#gallery-trigger",
-            start: `top+=${
-              sections.length * fadeInOffset +
-              fadeOutDelay +
-              index * fadeOutOffset
-            }vh 85%`,
-            end: `+=${fadeDistance}vh`,
-            scrub: true,
-          },
-        });
-      });
   });
 
   return (
@@ -68,9 +50,9 @@ export default function HeroGallery() {
       >
         <div
           id="gallery-section"
-          className="flex-1 flex items-center justify-center bg-orange-500"
+          className="flex-1 flex items-center justify-center bg-orange-500 overflow-hidden"
         >
-          <Image fill src="https://picsum.photos/900?1" alt="Gallery 1" />
+          {showGallerySection1 && <GallerySection1 />}
         </div>
 
         <div
