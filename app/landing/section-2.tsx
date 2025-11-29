4cded73f7f2a2b9ac9eb7439f4a2e98e0de727be
Adrià Gomis <gomisroca@gmail.com>
2025-11-29 18:07:54 +0100

    improve sec2 text anim

--- app/landing/section-2.tsx
+++ app/landing/section-2.tsx
@@ -13,26 +13,28 @@ export default function TextWave() {
 
     const tl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
 
-    tl.from(split.chars, {
-      scale: 1.5,
-      duration: 0.6,
-      ease: "power3.out",
-      stagger: 0.05,
-    }).to(
+    tl.fromTo(
       split.chars,
       {
         scale: 1,
-        duration: 0.25,
-        ease: "power2.out",
       },
-      "<0.1"
-    );
+      {
+        scale: 1.15,
+        duration: 0.6,
+        ease: "power3.out",
+        stagger: 0.05,
+      }
+    ).to(split.chars, {
+      scale: 1,
+      duration: 0.25,
+      ease: "power2.out",
+    });
 
-    return () => split.revert();
+    return () => tl.kill();
   });
 
   return (
-    <div className="text-5xl font-extrabold p-10">
+    <div className="text-6xl font-extrabold p-10">
       <div ref={textRef}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       </div>
